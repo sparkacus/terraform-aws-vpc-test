baaa405c71a91b3f6c0f461205691f8bf0eacc19
Timothy Jarman <timothy.jarman@duedil.com>
2022-12-06 13:18:54 +0000

    Remove additional region

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # terraform-aws-vpc-test
 
-In this example, we're using Terraform modules to create basic network configurations and an EC2 instance in three AWS regions + peering them all together.
+In this example, we're using Terraform modules to create basic network configurations and an EC2 instance in two AWS regions + peering them together.
 
 Tested using:
 ```
@@ -58,21 +58,26 @@ Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
 
 Outputs:
 
-aws_instance_id_ireland = "i-09ddb408286b8ddd9"
-aws_instance_id_london = "i-000677f1c6e576b21"
-aws_instance_id_paris = "i-05dcc8f6f84ba1dbf"
-aws_instance_private_ip_ireland = "10.1.0.16"
-aws_instance_private_ip_london = "10.0.0.240"
-aws_instance_private_ip_paris = "10.2.0.142"
+aws_instance_id_ireland = "i-07be1acb1e60c4d8e"
+aws_instance_id_london = "i-0df7453e1647f53a4"
+aws_instance_private_ip_ireland = "10.1.0.145"
+aws_instance_private_ip_london = "10.0.0.64"
 ```
 
 
 Example connection; runs `ping` from the EC2 instance in the Ireland region to the EC2 instance in the London region
 ```
 # export EC2_ID_IRELAND=$(terraform output --raw aws_instance_id_ireland)
+# export EC2_ID_LONDON=$(terraform output --raw aws_instance_id_london)
+# export EC2_IP_IRELAND=$(terraform output --raw aws_instance_private_ip_ireland)
 # export EC2_IP_LONDON=$(terraform output --raw aws_instance_private_ip_london)
 
 # mssh -r eu-west-1 $EC2_ID_IRELAND 'ping $EC2_IP_LONDON -c 1 | head -n 2 | tail -1'
 
-64 bytes from 10.0.0.240: icmp_seq=1 ttl=255 time=13.8 ms
+64 bytes from 10.0.0.64: icmp_seq=1 ttl=255 time=12.8 ms
+
+
+# mssh -r eu-west-2 $EC2_ID_LONDON 'ping $EC2_IP_IRELAND -c 1 | head -n 2 | tail -1'
+
+64 bytes from 10.1.0.145: icmp_seq=1 ttl=255 time=11.3 ms
 ```
--- main.tf
+++ main.tf
@@ -17,11 +17,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-provider "aws" {
-  alias  = "paris"
-  region = "eu-west-3"
-}
-
 # Create network + EC2 instance in the London region
 module "london" {
   source     = "./modules/test-network"
@@ -34,13 +29,6 @@ module "ireland" {
   source     = "./modules/test-network"
   cidr_block = "10.1.0.0/16"
   providers  = { aws = aws.ireland }
-}
-
-# Create network + EC2 instance in the Paris region
-module "paris" {
-  source     = "./modules/test-network"
-  cidr_block = "10.2.0.0/16"
-  providers  = { aws = aws.paris }
 }
 
 # Peer the London and Ireland networks
@@ -62,47 +50,7 @@ module "peering-london-ireland" {
   }
 }
 
-# Peer the London and Paris networks
-module "peering-london-paris" {
-  source = "./modules/test-peering"
-
-  vpc_id      = module.london.vpc_id
-  peer_vpc_id = module.paris.vpc_id
-
-  cidr_block_requester = module.london.cidr_block
-  cidr_block_accepter  = module.paris.cidr_block
-
-  route_table_id_requester = module.london.route_table_id
-  route_table_id_accepter  = module.paris.route_table_id
-
-  providers = {
-    aws.accepter  = aws.paris
-    aws.requester = aws.london
-  }
-}
-
-# Peer the Ireland and Paris networks
-module "peering-ireland-paris" {
-  source = "./modules/test-peering"
-
-  vpc_id      = module.ireland.vpc_id
-  peer_vpc_id = module.paris.vpc_id
-
-  cidr_block_requester = module.ireland.cidr_block
-  cidr_block_accepter  = module.paris.cidr_block
-
-  route_table_id_requester = module.ireland.route_table_id
-  route_table_id_accepter  = module.paris.route_table_id
-
-  providers = {
-    aws.accepter  = aws.paris
-    aws.requester = aws.ireland
-  }
-}
-
 output "aws_instance_id_london" { value = module.london.aws_instance.id }
 output "aws_instance_private_ip_london" { value = module.london.aws_instance.private_ip }
 output "aws_instance_id_ireland" { value = module.ireland.aws_instance.id }
 output "aws_instance_private_ip_ireland" { value = module.ireland.aws_instance.private_ip }
-output "aws_instance_id_paris" { value = module.paris.aws_instance.id }
-output "aws_instance_private_ip_paris" { value = module.paris.aws_instance.private_ip }
